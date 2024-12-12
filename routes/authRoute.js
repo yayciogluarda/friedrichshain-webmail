d7f8186e0b1e7bce4031fb84d4bb293bb754bf1f
yayciogluarda <yayciogluarda@gmail.com>
2024-12-12 17:56:40 +0100

    Update authRoute.js

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,27 +3,27 @@ const router = express.Router();
 
 // Hardcoded login credentials
 const userCredentials = {
-    email: "laura.heisenberg@polizei-friedrichshain21.de",
-    password: "gr3t@07102012MAIL"
+  email: "laura.heisenberg@polizei-friedrichshain21.de",
+  password: "gr3t@07102012MAIL"
 };
-
-
 
 // Routes
 router.get("/", (req, res) => {
-    res.render("login", { error: null });
+  res.render("login", { error: null });
 });
 
 router.post("/login", (req, res) => {
-    const { email, password } = req.body;
-  
-    if (email === userCredentials.email && password === userCredentials.password) {
-      res.render("emails", { emails });
-    } else {
-      res.render("login", { error: "Invalid email or password." });
-    }
-  });
-  
+  const { email, password } = req.body;
+
+  if (email === userCredentials.email && password === userCredentials.password) {
+    // Mark the user as logged in
+    req.session.isLoggedIn = true;
+
+    // Redirect to the emails page
+    res.redirect("/emails");
+  } else {
+    res.render("login", { error: "Invalid email or password." });
+  }
+});
 
 export default router;
-
